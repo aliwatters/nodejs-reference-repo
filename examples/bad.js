1b195f384b4ffd33c83b8943f9495e7e5be27ffd
Ali Watters <awatters@gs-mbp13-awatters.local>
2016-01-12 15:43:26 -0800

    a deliberatly "bad" js file

---
+++ examples/bad.js
@@ -0,0 +1,12 @@
+function example()
+{ // on newline - we don't want this
+	// doesn't conform to coding standards or style!
+	if (bad == false) { // only double eq
+            // bad indentation
+		console.log("everything is awful!") // double quoted - we want single
+	}
+
+	var bad = false; // not hoisted
+}
+
+example
