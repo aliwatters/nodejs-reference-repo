4210b714e2dc5ed0d217032def9e56440ffc2b12
Ali Watters <ali.watters@gmail.com>
2016-01-14 10:54:10 -0800

    test

--- examples/bad.js
+++ examples/bad.js
@@ -1,12 +1,11 @@
-function example()
-{ // on newline - we don't want this
-	// doesn't conform to coding standards or style!
-	if (bad == false) { // only double eq
-            // bad indentation
-		console.log("everything is awful!") // double quoted - we want single
-	}
+function example() { // on newline - we don't want this
+  var bad = false; // not hoisted
+  // doesn't conform to coding standards or style!
+  if (bad === false) { // only double eq
+    // bad indentation
+    console.log('everything is awful!'); // double quoted - we want single
+  }
 
-	var bad = false; // not hoisted
 }
 
-example() // and no trailing line on EOF
+example(); // and no trailing line on EOF
